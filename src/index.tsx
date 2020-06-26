54cba3788a83675cc913073d441e3f1f9d6bc850
kothing <kothing@outlook.com>
2020-06-26 15:28:17 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,38 @@ import { HelmetProvider } from 'react-helmet-async';
 import './locales/i18n';
 /* -------------------------------------------------------------------------- */
 
+/*
+const MOUNT_NODE = document.getElementById('root') as HTMLElement;
+
+interface Props {
+  Component: typeof App;
+}
+
+const ConnectedApp = ({ Component }: Props) => (
+  <HelmetProvider>
+    <React.StrictMode>
+      <Component />
+    </React.StrictMode>
+  </HelmetProvider>
+);
+
+const render = (Component: typeof App) => {
+  ReactDOM.render(<ConnectedApp Component={Component} />, MOUNT_NODE);
+};
+
+if (module.hot) {
+  // Hot reloadable translation json files and app
+  // modules.hot.accept does not accept dynamic dependencies,
+  // have to be constants at compile-time
+  module.hot.accept(['./app', './locales/i18n'], () => {
+    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+    import('./app').then((mod: any) => render(mod.default));
+  });
+}
+
+render(App);
+*/
+
 ReactDOM.render(
   <HelmetProvider>
     <React.StrictMode>
